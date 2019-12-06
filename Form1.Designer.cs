75e0e5276d7b19effec7fe9b530a97d44c77e7e1
koland12201 <koland12201@yahoo.com.hk>
2019-12-06 17:22:40 +0800

    tim561 scanner tcp completed wip: convert to planar data

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace MSC_control
+﻿using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MSC_control
 {
     partial class Form1
     {
@@ -28,84 +30,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Length = new System.Windows.Forms.Label();
-            this.textBox_Length = new System.Windows.Forms.TextBox();
-            this.textBox_Width = new System.Windows.Forms.TextBox();
-            this.Width = new System.Windows.Forms.Label();
-            this.textBox_Height = new System.Windows.Forms.TextBox();
-            this.Height = new System.Windows.Forms.Label();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label23 = new System.Windows.Forms.Label();
             this.label24 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label25 = new System.Windows.Forms.Label();
-            this.textBox_Barcode = new System.Windows.Forms.TextBox();
-            this.Barcode = new System.Windows.Forms.Label();
-            this.textBox_Weight = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox_Volume = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox_Index = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.listBox_Barocde = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Length
-            // 
-            this.Length.AutoSize = true;
-            this.Length.Location = new System.Drawing.Point(305, 92);
-            this.Length.Name = "Length";
-            this.Length.Size = new System.Drawing.Size(38, 12);
-            this.Length.TabIndex = 0;
-            this.Length.Text = "Length";
-            // 
-            // textBox_Length
-            // 
-            this.textBox_Length.Enabled = false;
-            this.textBox_Length.HideSelection = false;
-            this.textBox_Length.Location = new System.Drawing.Point(278, 116);
-            this.textBox_Length.Name = "textBox_Length";
-            this.textBox_Length.Size = new System.Drawing.Size(100, 22);
-            this.textBox_Length.TabIndex = 1;
-            // 
-            // textBox_Width
-            // 
-            this.textBox_Width.Enabled = false;
-            this.textBox_Width.Location = new System.Drawing.Point(32, 116);
-            this.textBox_Width.Name = "textBox_Width";
-            this.textBox_Width.Size = new System.Drawing.Size(100, 22);
-            this.textBox_Width.TabIndex = 3;
-            // 
-            // Width
-            // 
-            this.Width.AutoSize = true;
-            this.Width.Location = new System.Drawing.Point(58, 92);
-            this.Width.Name = "Width";
-            this.Width.Size = new System.Drawing.Size(34, 12);
-            this.Width.TabIndex = 2;
-            this.Width.Text = "Width";
-            // 
-            // textBox_Height
-            // 
-            this.textBox_Height.Enabled = false;
-            this.textBox_Height.Location = new System.Drawing.Point(157, 116);
-            this.textBox_Height.Name = "textBox_Height";
-            this.textBox_Height.Size = new System.Drawing.Size(100, 22);
-            this.textBox_Height.TabIndex = 5;
-            // 
-            // Height
-            // 
-            this.Height.AutoSize = true;
-            this.Height.Location = new System.Drawing.Point(183, 92);
-            this.Height.Name = "Height";
-            this.Height.Size = new System.Drawing.Size(36, 12);
-            this.Height.TabIndex = 4;
-            this.Height.Text = "Height";
             // 
             // label23
             // 
             this.label23.AutoSize = true;
             this.label23.Enabled = false;
-            this.label23.Location = new System.Drawing.Point(21, 293);
+            this.label23.Location = new System.Drawing.Point(10, 621);
             this.label23.Name = "label23";
             this.label23.Size = new System.Drawing.Size(70, 12);
             this.label23.TabIndex = 40;
@@ -115,7 +56,7 @@
             // 
             this.label24.AutoSize = true;
             this.label24.Enabled = false;
-            this.label24.Location = new System.Drawing.Point(97, 293);
+            this.label24.Location = new System.Drawing.Point(86, 621);
             this.label24.Name = "label24";
             this.label24.Size = new System.Drawing.Size(41, 12);
             this.label24.TabIndex = 41;
@@ -123,7 +64,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(203, 289);
+            this.textBox1.Location = new System.Drawing.Point(192, 617);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(175, 22);
             this.textBox1.TabIndex = 46;
@@ -132,144 +73,66 @@
             // label25
             // 
             this.label25.AutoSize = true;
-            this.label25.Location = new System.Drawing.Point(161, 292);
+            this.label25.Location = new System.Drawing.Point(150, 620);
             this.label25.Name = "label25";
             this.label25.Size = new System.Drawing.Size(15, 12);
             this.label25.TabIndex = 47;
             this.label25.Text = "IP";
             // 
-            // textBox_Barcode
+            // chart1
             // 
-            this.textBox_Barcode.Enabled = false;
-            this.textBox_Barcode.HideSelection = false;
-            this.textBox_Barcode.Location = new System.Drawing.Point(157, 53);
-            this.textBox_Barcode.Name = "textBox_Barcode";
-            this.textBox_Barcode.Size = new System.Drawing.Size(100, 22);
-            this.textBox_Barcode.TabIndex = 49;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1116, 599);
+            this.chart1.TabIndex = 48;
+            this.chart1.Text = "chart1";
+            this.chart1.Series[0].ChartType = SeriesChartType.Point;
             // 
-            // Barcode
+            // button1
             // 
-            this.Barcode.AutoSize = true;
-            this.Barcode.Location = new System.Drawing.Point(183, 29);
-            this.Barcode.Name = "Barcode";
-            this.Barcode.Size = new System.Drawing.Size(44, 12);
-            this.Barcode.TabIndex = 50;
-            this.Barcode.Text = "Barcode";
-            // 
-            // textBox_Weight
-            // 
-            this.textBox_Weight.Enabled = false;
-            this.textBox_Weight.Location = new System.Drawing.Point(157, 188);
-            this.textBox_Weight.Name = "textBox_Weight";
-            this.textBox_Weight.Size = new System.Drawing.Size(100, 22);
-            this.textBox_Weight.TabIndex = 54;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(176, 164);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 12);
-            this.label1.TabIndex = 53;
-            this.label1.Text = "Weight";
-            // 
-            // textBox_Volume
-            // 
-            this.textBox_Volume.Enabled = false;
-            this.textBox_Volume.Location = new System.Drawing.Point(32, 188);
-            this.textBox_Volume.Name = "textBox_Volume";
-            this.textBox_Volume.Size = new System.Drawing.Size(100, 22);
-            this.textBox_Volume.TabIndex = 52;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 164);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 12);
-            this.label2.TabIndex = 51;
-            this.label2.Text = "Volume";
-            // 
-            // textBox_Index
-            // 
-            this.textBox_Index.Enabled = false;
-            this.textBox_Index.HideSelection = false;
-            this.textBox_Index.Location = new System.Drawing.Point(32, 53);
-            this.textBox_Index.Name = "textBox_Index";
-            this.textBox_Index.Size = new System.Drawing.Size(100, 22);
-            this.textBox_Index.TabIndex = 55;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(61, 29);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 12);
-            this.label3.TabIndex = 56;
-            this.label3.Text = "Index";
-            // 
-            // listBox_Barocde
-            // 
-            this.listBox_Barocde.FormattingEnabled = true;
-            this.listBox_Barocde.ItemHeight = 12;
-            this.listBox_Barocde.Location = new System.Drawing.Point(384, 29);
-            this.listBox_Barocde.Name = "listBox_Barocde";
-            this.listBox_Barocde.Size = new System.Drawing.Size(101, 244);
-            this.listBox_Barocde.TabIndex = 57;
+            this.button1.Location = new System.Drawing.Point(414, 621);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 49;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 334);
-            this.Controls.Add(this.listBox_Barocde);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox_Index);
-            this.Controls.Add(this.textBox_Weight);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox_Volume);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.Barcode);
-            this.Controls.Add(this.textBox_Barcode);
+            this.ClientSize = new System.Drawing.Size(1140, 652);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.label25);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label24);
             this.Controls.Add(this.label23);
-            this.Controls.Add(this.textBox_Height);
-            this.Controls.Add(this.Height);
-            this.Controls.Add(this.textBox_Width);
-            this.Controls.Add(this.Width);
-            this.Controls.Add(this.textBox_Length);
-            this.Controls.Add(this.Length);
             this.Name = "Form1";
             this.Text = "CLV62x barcode scanner";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label Length;
-        private System.Windows.Forms.TextBox textBox_Length;
-        private System.Windows.Forms.TextBox textBox_Width;
-        private System.Windows.Forms.Label Width;
-        private System.Windows.Forms.TextBox textBox_Height;
-        private System.Windows.Forms.Label Height;
         private System.Windows.Forms.Label label23;
         private System.Windows.Forms.Label label24;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label25;
-        private System.Windows.Forms.TextBox textBox_Barcode;
-        private System.Windows.Forms.Label Barcode;
-        private System.Windows.Forms.TextBox textBox_Weight;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox_Volume;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox_Index;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListBox listBox_Barocde;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,43 @@ using System.Text;
 using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Net;
+/*data stream
+ *Example            def
+ *<STX>              -1        Header
+ *sRA                 0        sRA
+ *LMDscandata         1        LMDscandata
+ *1                   2        version number
+ *1                   3        device number 
+ *114D00A             4        serial number
+ *0 0                 5-6      device status (0 OK, 1 error, 2 pollution warning, pollution error) x2
+ *2B26                7        telegram counter
+ *2B9A                8        scan counter
+ *744FCC8E            9        time since startup
+ *744FE371           10        time of transmission
+ *0 0                11-12     status of digital input  x2
+ *1 0                13-14     status of digital output x2
+ *0                  15        reserved
+ *5DC                16        scan freq
+ *A2                 17        measurement freq
+ *0                  18        amount of encoder(=0, no encoder)
+ *1                  19        amount of 18b channels
+ *DIST1              20        content (DIST1, RSSI1, DIST2, RSSI2)
+ *3F800000           21        scale factor
+ *00000000           22        scale factor offset
+ *FFF92230           23        start angle
+ *D05                24        steps
+ *32B                25        amount of data
+ *[example data]     26-836    data(0000-FFFF)     [example data]: 1C6 1C3 1C0 1C2 1C2 1CD 1C7 1CF 1CA 1CC 1C7 1C7 1CA 1CB 1BD 1BC 1BA 1B9 1B4 1B0 1B8 1B9 1B5 1B0 1BA 1AF 1B9 1B0 1BE 1B6 1B1 1BA 1B1 1B8 1BC 1C4 1C6 1CA 1CD 1D1 1CA 1D2 1C6 173 630 606 608 60F 616 60C 610 618 619 624 618 629 626 63A 63E 63D 643 639 614 610 643 65B 64D 66F 642 5F4 5D9 5C8 5F2 5F8 5FB 622 61B 5DC 5B6 595 58F 5B3 622 654 613 59C 594 573 536 524 51E 51E 532 52F 547 58A 546 4CD 4CA 4CF 4C5 4C8 4B8 4C1 4CF 4DD 4CA 4A8 4A5 4A7 498 492 497 4AB 4BA 4C7 4D2 4E6 4F4 508 514 4F5 4D2 4DD 4CF 4C9 507 50E 512 515 518 4F8 499 48E 486 465 474 46B 466 48C 486 4A4 4CD 4DB 4BF 475 468 463 474 476 485 487 481 494 4DD 4A8 451 445 445 43F 458 486 4A5 4B6 49C 43B 43B 44A 476 492 4D9 4DF 486 493 4AB 4D1 522 52C 50F 4E5 4DB 4BE 4BB 4CC 4B3 4A7 4A4 48F 48C 44E 435 43D 43F 43B 43E 43B 442 43F 43E 4F0 BC8 BD1 C03 C46 C37 C3B C73 CB4 CDF CD9 CBA C88 C3F C1E B61 AC3 ACF BD8 C33 CD5 C82 C1E C1A C26 C2D C7C CDF CC4 C90 A7C A41 A2C A21 A1C A15 99C 8F0 8DD 8CF 8B9 8B1 8B2 8BE 8C7 8C3 8BE 8AD 8A8 8A6 8A0 8A0 8AE 8A2 89F 898 88D 885 889 884 886 87E 899 8B0 8A9 898 88C 88A 89F 8B7 8B9 8C6 8E0 8E2 8D8 8D0 8F0 A2B AD6 AC0 ABD AC0 ABE AC7 ACC AD1 AD5 ADC AE3 AE7 AEE AF5 AFD B05 B10 B18 B1E B2A B2F B38 B3F B48 B52 B59 B62 B68 B72 B7E B79 B62 B46 B21 B08 AE9 ACD AB0 9F1 913 8FB 8E0 8CE 8B8 8A1 88B 87C 868 856 840 82F 81C 80D 7FC 7EB 7DC 7C8 7C0 7BF 7CD 7D1 833 3A2 36D 2FF 2E1 2E4 2E3 2E9 2E0 2C9 2B8 2B9 298 27D 27E 275 283 274 276 27B 273 27B 27B 276 270 278 274 271 27C 277 285 275 287 27D 288 28A 286 293 28C 28A 284 28B 289 27E 27D 277 274 276 27C 26E 274 274 275 27D 288 304 303 2BE 284 27C 282 2CD 342 36E 359 36D 391 39B 38B 32D 2F0 2F3 30C 32D 324 326 325 32E 326 321 30E 2E4 2AE 287 2A0 2DD 303 312 314 308 30D 307 304 305 2FF 2FD 2FC 31A 3A9 3E5 3E8 3F1 3F1 3DE 3EA 3EE 3E8 3E7 3DF 3DA 3D4 3D2 3D4 3D2 3CA 3D9 3CC 3D3 3CD 3CD 3C5 3CC 3BD 3B7 3B7 3C6 498 5CC 5E1 5DF 5E2 5EF 5E7 5E7 5EB 5EE 5E4 4DD 3F6 3DF 3CE 3BE 3AE 3A0 393 385 37B 36C 35F 355 349 33D 331 324 31C 312 30B 2FC 2F2 2EB 2E8 2DE 2D0 2CE 2C2 2BA 2B3 2A8 2A9 29E 297 28E 286 281 281 27C 27A 26C 261 263 258 251 24F 245 236 229 231 233 239 23E 243 244 246 250 241 250 24D 24F 25A 25F 255 25B 262 262 264 26C 246 227 215 250 26A 27C 278 281 287 299 296 299 295 297 2A1 2A2 2A2 2AC 2B7 2B9 2BB 2C3 2CB 2CC 2C5 2D3 2D6 2E0 2DA 2DC 2DF 2E8 2F6 2F4 2FF 308 310 313 31B 322 326 32F 338 337 33F 34A 34B 357 35B 367 36E 378 36E 37E 0 718 D7 109 11F 11F 11C 118 119 116 123 11C 114 113 112 113 119 114 111 116 10D 10F 10E 109 10B 10C 107 10E 10C 105 10D 10A 10E 10D 10A 106 109 109 101 106 FC 103 100 101 108 106 100 104 105 102 FC 106 FA FE FE FB 100 107 F9 FE F7 FD 101 F6 F5 F8 F8 FC FF F6 F8 F9 FC FF F8 FE FC FE 108 10C 112 111 126 14C 25D 28A 294 299 29C 29B 297 290 28D 281 276 248 1B1 15C 129 1CF 265 28C 2A7 2A2 29B 29C 2A2 2A7 2AA 2A6 2A2 2A4 2A5 2A5 2A5 2A0 2A7 2A6 29E 2A8 2AA 2A5 2A4 2AB 2AA 2A2 2A5 28C 267 22C 265 280 290 2A7 2AB 28E 237 21E 211 1E1 1AD 1A2 19B 198 1A0 1A1 1A4 1C0 1E9 207 213 20F 215 21D 213 222 24A 2AD 2CD 2CF 2D5 2CD 2D2 2D7 2D8 2CC 2B4 2AE 2A7 2AC 2AE 2AB 2A6 2AA 2A4 2A8 291 28A 288 2A2 2A5 2AC 29E 296 273 260 2B7 301 310 312 30D 308 314 325 321 323 334 32F 33C 33C 33C 34A 342 33D 33F 321 307 2F6 2F2 2F8 2F8 2ED 2F4
+ *0                 837        amount of 8b data (=0, no 8b data)
+ *0                 838        position data (=0, no IMU)
+ *1                 839        device name (0= no mane, 1= have name)
+ *B                 840        device name length (0-F)
+ *"not defined"     841        device name
+ *0                 842        comment (0= no comment, 1= have comment)
+ *0                 843        time transmitted (0= no time transmitted, 1= have time transmitted)
+ *0                 844        event info (0= no event info, 1= have event info)
+ *<ETX>             845        checksum
+ */
 
 namespace MSC_control
 {
@@ -15,8 +52,12 @@ namespace MSC_control
     {
         Socket client;//socket
         byte[] bufbar = new byte[10240];
-        String[] barcodeArry = new String[1000];
-        int barIndex = 0;
+        int[] X = new int[1000];
+        double[] scanData = new double[1000];
+        int dataLength = 0;
+        //flags
+        bool replied = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -29,7 +70,6 @@ namespace MSC_control
             string ip = Properties.Settings.Default.ip_set;
             this.textBox1.Text = ip;
 
-   
             int port = Convert.ToInt16(Properties.Settings.Default.port_set);
             IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(ip), port);
             client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -41,52 +81,12 @@ namespace MSC_control
             }
             catch (Exception)
             {
-                
                // throw;
             }
-            try //enable data feed
-            {
-                String command = "sEI C1 1";
-                Byte[] commad_arry = Encoding.UTF8.GetBytes(command);
-
-                Byte[] send = new Byte[command.Length + 2];
-                send[0] = 0x02;
-                for (int i = 0; i < command.Length; i++)
-                {
-                    send[i + 1] = commad_arry[i];
-                }
-
-                send[command.Length + 1] = 0x03;
-
-                client.Send(send);
-                System.Threading.Thread.Sleep(10);
-            }
-            catch (Exception)
-            {
-
-               // throw;
-            }
-            try //activate module
-            {
-                String command = "sMI A3"; //"sMI A4" to shut down
-                Byte[] commad_arry = Encoding.UTF8.GetBytes(command);
-
-                Byte[] send = new Byte[command.Length + 2];
-                send[0] = 0x02;
-                for (int i = 0; i < command.Length; i++)
-                {
-                    send[i + 1] = commad_arry[i];
-                }
-
-                send[command.Length + 1] = 0x03;
-
-                client.Send(send);
-                System.Threading.Thread.Sleep(10);
-            }
-            catch (Exception)
-            {
-               //throw;
-            }
+            //init
+            sendCommand("sMN SetAccessMode 03 F4724744");   //login
+            sendCommand("sMI 3E");                          //enable scan, 3F to disable
+            sendCommand("sMI 2");                           //logout, run
 
         }
 
@@ -103,31 +103,34 @@ namespace MSC_control
             parsedata(_context);
 
         }
-        
+
+        void sendCommand(String command)
+        {
+            Byte[] commad_arry = Encoding.UTF8.GetBytes(command);
+            Byte[] send = new Byte[command.Length + 2];
+            send[0] = 0x02;
+            for (int i = 0; i < command.Length; i++)
+            {
+                send[i + 1] = commad_arry[i];
+            }
+            send[command.Length + 1] = 0x03;
+
+            client.Send(send);
+            System.Threading.Thread.Sleep(10);
+        }
+
         void parsedata(String data) 
         {
             String[] _commandArry = data.Split(' ');
-            if (_commandArry[0] == "sSI")
+            if (_commandArry[0] == "sRA"&&_commandArry[1]== "LMDscandata")
             {
-                try
+                dataLength = Convert.ToInt32(_commandArry[25], 16);
+                for (int i_para=0;i_para< dataLength; i_para++)
                 {
-                    
-                    textBox_Index.Text = barIndex.ToString();
-                    textBox_Barcode.Text = _commandArry[19]; //barcode
-                    //textBox_Length.Text = _commandArry[2];  //length
-                    //textBox_Width.Text = _commandArry[3];   //width
-                    //textBox_Height.Text = _commandArry[4];  //height
-                    //textBox_Volume.Text = _commandArry[5];  //volume
-                    //textBox_Weight.Text = _commandArry[6];  //weight
-                    listBox_Barocde.Items.Add(_commandArry[19]);
-                    barcodeArry[barIndex] = _commandArry[19];
-                    barIndex += 1;
+                    scanData[i_para] = Convert.ToInt32(_commandArry[i_para + 26],16);
+                    X[i_para] = i_para;
                 }
-                catch (Exception)
-                {
-                    textBox_Barcode.Text = "No Read";
-                    //throw;
-                }
+                replied = true;
             }
         }
 
@@ -137,9 +140,32 @@ namespace MSC_control
             {
                 Properties.Settings.Default.ip_set = textBox1.Text;
                 Properties.Settings.Default.Save();
-
             }
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                replied = false;
+                chart1.Series[0].Points.Clear();
+                sendCommand("sRN LMDscandata 0");               //request scan data once
+                while (replied == false)
+                {
+                    System.Threading.Thread.Sleep(10);
+                }
+
+                for (int i_btn=0; i_btn<= dataLength; i_btn++)
+                {
+                    if(i_btn<dataLength/2)
+                    {
+                       
+                    }
+                    else if(i_btn> dataLength / 2)
+                    {
+                    
+                    }
+                }
+                chart1.Series[0].Points.DataBindXY(X, scanData);
+        }
     }
 }
